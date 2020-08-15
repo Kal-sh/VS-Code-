a300cede27d784b60c3a533d74e3aa2367d70bed
Kaleab cherinet <kalecherinet@gmail.com>
2020-08-15 13:55:03 +0300

    cat

--- SQL/Test.sql
+++ SQL/Test.sql
@@ -15,6 +15,21 @@ INSERT into Students VALUES
 SELECT Sage FROM Students
 WHERE Sage >= 24;
 
+drop table cat;
+
+CREATE TABLE cat(
+    Cid int NOT NULL
+    PRIMARY key IDENTITY,
+    Cname VARCHAR(20) NOT NULL,
+    Cage int NOT NULL
+);
+
+INSERT INTO cat VALUES
+('mello', 5),
+('yakk', '3'),
+('boo', '4')
+
+SELECT * FROM cat;
 
 
 
