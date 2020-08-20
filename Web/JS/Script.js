05442edf08cb72e171659892891363a64e13f9a2
Kaleab cherinet <kalecherinet@gmail.com>
2020-08-20 15:46:43 +0300

    assignment

--- Web/JS/Script.js
+++ Web/JS/Script.js
@@ -1,3 +1,7 @@
-var userName = prompt("what is ur name");
-alert("nice to meet u, " + userName);
-console.log("nice to meet u, " + userName);
+var userName1 = prompt("what is ur First Name?");
+var userName2 = prompt("what is ur Last Name?");
+var userAge = prompt("How old are you?");
+alert("nice to meet u, " + userName1 +" "+ userName2);
+alert("you are " + userAge + " Years old.");
+console.log("nice to meet u, " + userName +" "+ userName2);
+console.log("you are " + userAge + " Years old.");
