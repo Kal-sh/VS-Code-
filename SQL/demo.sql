2bb90a7198af081ad8493c5a2d968e47420d4fdc
Kaleab cherinet <kalecherinet@gmail.com>
2020-09-18 10:49:53 +0300

    exercise

--- SQL/demo.sql
+++ SQL/demo.sql
@@ -43,9 +43,9 @@ CREATE TABLE staff(
     sname VARCHAR(10) NOT NULL,
     email VARCHAR(20) NOT NULL,    
 );
-
+drop table Advisor;
 CREATE TABLE Advisor(
-    Aid int IDENTITY,
+    Aid int PRIMARY KEY IDENTITY,
     Aname VARCHAR(10) NOT NULL,
     adv INT
 );
@@ -61,3 +61,103 @@ INSERT INTO Advisor VALUES
     ('Dr gebre', NULL)
 
 SELECT sname AS [students name] from staff;
+
+
+ALTER table staff drop COLUMN sage;
+ALTER TABLE staff
+add Sinfo int;
+
+ALTER table staff ALTER COLUMN sage VARCHAR(20);
+
+sp_help staff; 
+
+ALTER table staff ALTER COLUMN sage int IDENTITY(2,4);
+
+SELECT * FROM staff;
+SELECT * FROM Advisor;
+
+ALTER TABLE staff
+add staff_info int;
+
+ALTER table staff
+add CONSTRAINT st_fn
+FOREIGN KEY (staff_info) REFERENCES Advisor(Aid);
+
+insert into staff VALUES
+('chala','ch@gmail.com',2)
+
+CREATE table stu_council(
+    id int PRIMARY KEY IDENTITY,
+    adress VARCHAR(20),
+    email VARCHAR(50),
+    stu_info int
+)
+
+
+update staff set age=21
+where sid=6
+
+ALTER table stu_council 
+ADD CONSTRAINT fn_stu
+FOREIGN KEY (stu_info) REFERENCES Advisor(Aid);
+
+SELECT * FROM Advisor;
+SELECT * FROM stu_council;
+SELECT * FROM staff;
+
+UPDATE stu_council set email='kk@gmal.com'
+WHERE id=2
+
+
+insert into stu_council VALUES
+    ('bole','ab@gmail.com',3)
+
+ALTER table staff
+add CONSTRAINT email
+UNIQUE(email);
+
+alter TABLE staff
+
+ALTER TABLE staff
+ADD CONSTRAINT default_name
+DEFAULT deeee for sname
+
+SELECT *from staff
+
+alter table staff
+add age int
+
+ALTER TABLE staff
+add CONSTRAINT default_age
+DEFAULT 20 for age
+
+INSERT into staff (sname,email,staff_info) VALUES
+('gaga','gg@gmail.com',1)
+
+
+
+
+
+CREATE table Class(
+    cid int not null PRIMARY key IDENTITY,
+    name VARCHAR(20) not NULL,
+    department int NULL
+)
+
+create table Department(
+    did int not null PRIMARY KEY IDENTITY,
+    dname VARCHAR(50) not null
+)
+
+ALTER table Class
+add CONSTRAINT class_fn
+FOREIGN KEY (department) REFERENCES Department(did);
+
+insert into class VALUES
+('kebede',1)
+
+INSERT into Department VALUES
+('uaaa')
+
+SELECT * from Class;
+
