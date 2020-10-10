74e4353d8681973f5fb8aa0cd3ed2fa708412d3f
Kaleab cherinet <kalecherinet@gmail.com>
2020-10-10 19:11:29 +0300

    nothin

--- Web/JS/Script.js
+++ Web/JS/Script.js
@@ -1,9 +1,34 @@
-// var userName1 = prompt("what is ur First Name?");
-// var userName2 = prompt("what is ur Last Name?");
-// var userAge = prompt("How old are you?");
-// alert("nice to meet u, " + userName1 +" "+ userName2);
-// alert("you are " + userAge + " Years old.");
+/*
+var userName1 = prompt("what is ur First Name?");
+var userName2 = prompt("what is ur Last Name?");
+var userAge = prompt("How old are you?");
+alert("nice to meet u, " + userName1 +" "+ userName2);
+alert("you are " + userAge + " Years old.");
 console.log("nice to meet u, " + userName1 +" "+ userName2);
 console.log("you are " + userAge + " Years old.");
-
 console.log(typeof userName1);
+*/
+
+
+//Basic operators
+var year, yearJohn, yearMark;
+year = 2020;
+ageJohn = 34;
+ageMark = 23;
+
+//Maths Operators
+yearJohn = year - ageJohn;
+yearMark = year - ageMark;
+
+console.log(yearJohn);
+console.log(yearMark);
+
+console.log(year + 2);
+console.log(year - 2);
+console.log(year * 2);
+console.log(year / 2);
+
+//Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
---
+++ Web/JS/app.js
@@ -0,0 +1,60 @@
+//Example 1
+/*
+let rating = 9;
+
+if (rating > 4) {
+	console.log('Great!');
+} else if (rating < 4) {
+	console.log('Not bad');
+}
+*/
+
+//Example 2
+/*
+let value = 4;
+if (value === 3) {
+	console.log('true');
+} else {
+	console.log('false');
+}
+*/
+
+//Example 3
+/*
+let num = 53;
+if (num % 2 != 0) {
+	console.log('ODD Number!');
+} else {
+	console.log('Even Number!');
+}
+*/
+
+//Example 4
+/*
+let highScore = 52333;
+let userScore = 2455;
+
+if (userScore >= highScore) {
+	console.log(`Congrats, You have the new highscore ${userScore}`);
+	highScore = userScore;
+} else {
+	console.log(
+		`Good game, but your score of ${userScore} haven't beat the current highscore of ${highScore}.`
+	);
+}
+*/
+
+//Example 5
+/*
+let password = prompt('Input password');
+
+if (password.length >= 8) {
+	if (password.indexOf(' ') === -1) {
+		console.log('successful!');
+	} else {
+		console.log('Space is not allowed');
+	}
+} else {
+	console.log('Short password!');
+}
+*/
