365baac40ad2a8321fe4b0a1902b4c89fd6df049
Kaleab cherinet <kalecherinet@gmail.com>
2021-06-12 21:15:10 +0300

    dummy code

---
+++ Java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        return val1 + val2;
+    }
+
+}
+
+static void label(2,4)
+int val1,val2;
