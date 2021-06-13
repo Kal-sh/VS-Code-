ef0b6c32ae9fe8abd26c9cfaa9dcc41a952fdc6d
Kaleab cherinet <kalecherinet@gmail.com>
2021-06-13 09:04:30 +0300

    edit for loop

--- Java/shit.java
+++ Java/shit.java
@@ -1,7 +1,7 @@
 public class shit {
     public static void main(String[] args) {
         int results = 0;
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 5; i++) {
             if (i == 3) {
                 results += 10;
             } else {
