deb5121e55491e2d142a73bba93e37008c5e97b3
Kaleab cherinet <kalecherinet@gmail.com>
2021-06-16 15:13:34 +0300

    Data types min and max value

---
+++ Java/DataTypes.java
@@ -0,0 +1,49 @@
+// import java.util.Scanner;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // double myValue;
+
+        // Scanner value = new Scanner(System.in);
+        // System.out.print("Please input value :- ");
+        // myValue = value.nextDouble();
+
+        // boolean myBooleanMaxValue = Boolean.MAX_VALUE;
+        // boolean myBooleanMinValue = Boolean.MIN_VALUE;
+        // char myCharMinValue = Character.MIN_VALUE;
+        // char myCharMaxValue = Character.MAX_VALUE;
+        byte myByteMinValue = Byte.MIN_VALUE;
+        byte myByteMaxValue = Byte.MAX_VALUE;
+        short myShortMinValue = Short.MIN_VALUE;
+        short myShortMaxValue = Short.MAX_VALUE;
+        int myIntMinValue = Integer.MIN_VALUE;
+        int myIntMaxValue = Integer.MAX_VALUE;
+        long myLongMinValue = Long.MIN_VALUE;
+        long myLongMaxValue = Long.MAX_VALUE;
+        float myFloatMinValue = Float.MIN_VALUE;
+        float myFloatMaxValue = Float.MAX_VALUE;
+        double myDoubleMinValue = Double.MIN_VALUE;
+        double myDoubleMaxValue = Double.MAX_VALUE;
+
+        // System.out.println("Char Min Value = " + myCharMinValue);
+        // System.out.println("Char Max Value = " + myCharMaxValue);
+        System.out.println("Byte Min Value = " + myByteMinValue);
+        System.out.println("Byte Max Value = " + myByteMaxValue);
+        System.out.println("Short Min Value = " + myShortMinValue);
+        System.out.println("Short Max Value = " + myShortMaxValue);
+        System.out.println("Integer Min Value = " + myIntMinValue);
+        System.out.println("Integer Max Value = " + myIntMaxValue);
+        System.out.println("Long Min Value = " + myLongMinValue);
+        System.out.println("Long Max Value = " + myLongMaxValue);
+        System.out.println("Float Min Value = " + myFloatMinValue);
+        System.out.println("Float Max Value = " + myFloatMaxValue);
+        System.out.println("Double Min Value = " + myDoubleMinValue);
+        System.out.println("Double Max Value = " + myDoubleMaxValue);
+
+        // if (myValue > myIntMaxValue) {
+        // System.out.println("The value is Integer");
+        // } else {
+        // System.out.println("The value is out of bound");
+        // }
+    }
+}
