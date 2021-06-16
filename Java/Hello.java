5d17e9a372ab9c70b0ad655ef9700e544ff92534
Kaleab cherinet <kalecherinet@gmail.com>
2021-06-16 15:13:51 +0300

    Hello world

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World, this is a demo java program.");
+        System.out.println("Hello World");
     }
 }
