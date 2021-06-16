f4fa925fa0169c98c6bda3b47f2a1e9932d8fea2
Kaleab cherinet <kalecherinet@gmail.com>
2021-06-16 14:36:56 +0300

    Students grade update

--- Java/StudentsGrade.java
+++ Java/StudentsGrade.java
@@ -2,31 +2,44 @@ import java.util.Scanner;
 
 public class StudentsGrade {
     public static void main(String[] args) {
+
         String name;
         String sex;
         int age;
         int grade;
+
+        // * Student name input from user
         Scanner stuName = new Scanner(System.in);
         System.out.print("Enter Student Name :- ");
         name = stuName.nextLine();
+
+        // * Student Age input from user
         Scanner stuAge = new Scanner(System.in);
         System.out.print("Enter Student Age :- ");
         age = stuAge.nextInt();
+
+        // * Student Sex input from user
         Scanner stuSex = new Scanner(System.in);
         System.out.print("Enter Student Sex :- ");
         sex = stuSex.next();
+
+        // * Student Grade input from user
         Scanner stuGrade = new Scanner(System.in);
         System.out.print("Enter Student grade :- ");
         grade = stuGrade.nextInt();
 
         if (grade >= 90) {
-            System.out.println("Hi " + name + ", your grade is " + grade + ". Excellent!");
+            System.out.println("Student " + name + ", Gender " + sex + ", Age " + age + ", your grade is " + grade
+                    + ". Excellent!");
         } else if (grade > 80 && grade < 90) {
-            System.out.println("Hi " + name + ", your grade is " + grade + ". Good!");
+            System.out.println(
+                    "Student " + name + ", Gender " + sex + ", Age " + age + ", your grade is " + grade + ". Good!");
         } else if (grade < 70 && grade > 60) {
-            System.out.println("Hi " + name + ", your grade is " + grade + ". Not Bad!");
+            System.out.println(
+                    "Student " + name + ", Gender " + sex + ", Age " + age + ", your grade is " + grade + ". Not Bad!");
         } else {
-            System.out.println("Hi " + name + ", your grade is " + grade + ". Needs Improvement!");
+            System.out.println("Student " + name + ", Gender " + sex + ", Age " + age + ", your grade is " + grade
+                    + ". Needs Improvement!");
         }
 
     }
