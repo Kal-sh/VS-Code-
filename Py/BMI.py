1e9dcc23131372383328aa90de98d362c79f5c36
Kal-sh <Kalecherinet@gmail.com>
2024-09-15 20:22:51 +0300

    "Refactored BMI calculator to use a reusable input function, added input validation, and updated BMI categorization logic."

--- Py/BMI.py
+++ Py/BMI.py
@@ -1,18 +1,27 @@
 
 #! BMI Calculator
 
-height = float(input('What is your Height in m:\n'))
-weight = float(input('What is your Weight in kg:\n'))
+def get_float_input(prompt, min_value, max_value):
+    while True:
+        try:
+            value = float(input(prompt))
+            if value < min_value or value > max_value:
+                print(f"Invalid input. Please enter a value between {min_value} and {max_value}.")
+            else:
+                return value
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            
+height = get_float_input('What is your Height in m:\n', 0.5, 4.0)
+weight = get_float_input('What is your Weight in kg:\n',30,500)
 
-BMI = weight/(height**2)
-print(type(BMI))
-intBMI = int(BMI)
-# print(f'Your BMI is {int(BMI)}')
-if intBMI < 18.5:
-    print(f'Your BMI is {intBMI}, your are skinny AF.')
-elif intBMI > 25:
-    print(f'Your BMI is {intBMI}, your are on your way to be a fattie.')
-elif intBMI > 30:
-    print(f'Your BMI is {intBMI}, your are a fattie.')
+BMI = weight/height**2
+
+if BMI < 18.5:
+    print(f'Your BMI is {BMI:.2f}, your are skinny AF.')
+elif 18.5 <= BMI < 25:
+    print(f'Your BMI is {BMI:.2f}, your are on your way to be a fattie.')
+elif 25 <= BMI < 30:
+    print(f'Your BMI is {BMI:.2f}, your are a fattie.')
 else:
-    print(f'Your BMI is {intBMI}, your are fucked biggie.')
+    print(f'Your BMI is {BMI:.2f}, your are fucked piggie.')
