ff70ce37fe393d3c9330341c312088242c3825d7
Kal-sh <Kalecherinet@gmail.com>
2024-09-16 14:19:23 +0300

    "Refactored input handling for age to use a reusable function with validation"

--- Py/Death calc.py
+++ Py/Death calc.py
@@ -1,11 +1,24 @@
 
 #! Death Calculator
 
-age = int(input('how old are you? '))
+def get_int_input(prompt, min_value, max_value):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value < min_value or value > max_value:
+                print(f"Please enter a value between {min_value} and {max_value}.")
+            else:
+                return value
+        except ValueError:
+            print("Invalid input, please enter a valid number")
+            
+
+
+age = get_int_input("how old are you?\n", 1, 90)
 remaningYear = 90-age
 daysRemaining = remaningYear*365
-monthRemaining = remaningYear*12
 weeksRemaining = remaningYear*52
+monthRemaining = remaningYear*12
 
 print(
-    f'you have {daysRemaining} days, {weeksRemaining} weeks and {remaningYear} years left to Live.')
+    f'you have {daysRemaining} days, {weeksRemaining} weeks and {monthRemaining} months left to Live.')
