45554a211eed34a3e929ff4ae6e8d07ecbcc8db3
Kal-sh <Kalecherinet@gmail.com>
2024-09-22 01:22:55 +0300

    "Deleted Exercise1.py file"

--- Py/Exercise1.py
+++
@@ -1,7 +0,0 @@
-print('welcome to the tip calculator.')
-total_bill=float(input('what is the total bill?'))
-num_ppl=int(input('How many people to split the bill?'))
-tip=int(input('What percentage tip would you like to give?'))/100
-total_ammount=total_bill*(1+tip)
-personal_bill=total_ammount/num_ppl
-print(f'Each person should pay: {personal_bill:.2f}')
