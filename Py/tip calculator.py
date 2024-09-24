6280fc94e85d43ca72b298e36e624dfc5b8323e0
Kal-sh <Kalecherinet@gmail.com>
2024-09-24 21:04:29 +0300

    "Removed/added blank lines in main function"

--- Py/tip calculator.py
+++ Py/tip calculator.py
@@ -20,18 +20,18 @@ def calculate_personal_bill(total_bill, num_people):
 
 def main():
     print('welcome to the tip calculator.')
-    
+
     # Get user input
     total_bill = get_input_float('what is the total bill?\n', 1 , 1000000)
     num_people = get_input_float('How many people to split the bill?\n', 2 , 1000000)
     tip = get_input_float('What percentage tip would you like to give?\n', 0 , 100)
-    
+
     # Calculate the total bill and personal bill
     total_ammount = calculate_total_bill(total_bill,tip)
     personal_bill = calculate_personal_bill(total_ammount,num_people)
-    
+
     # Print result
     print(f'Each person should pay: {personal_bill:.2f}$')
-    
+
 if __name__ == '__main__':
     main()
