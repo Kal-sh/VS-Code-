e52d95ff237263d8fd904c05cdf03ebdb2c65216
Kal-sh <Kalecherinet@gmail.com>
2024-10-02 23:47:43 +0300

    "Refactored pizza order script to use a reusable function for yes/no input and simplified conditional statements."

--- Py/Pizza order.py
+++ Py/Pizza order.py
@@ -1,36 +1,35 @@
 print("Welcome to Python pizza Deliveries!")
+
 prices = {
     'small': {'pizza': 15, 'pepperoni': 2, 'extra_cheese': 1},
     'medium': {'pizza': 20, 'pepperoni': 3, 'extra_cheese': 1},
     'large': {'pizza': 25, 'pepperoni': 3, 'extra_cheese': 1}
 }
 
+def get_yes_or_no(prompt):
+    while True:
+        response = input(prompt).lower()
+        if response in ['yes', 'no']:
+            return response
+        print("Invalid input, please enter 'yes' or 'no'")
 
+#* Ask for the size of the pizza
 while True:
-    size = input("what size pizza do you want? (small, medium or large)\n").lower()
+    size = input("\nwhat size pizza do you want? (small, medium or large)\n").lower()
     if size in prices:
         break
     print("Invalid input, please enter 'small', 'medium', 'large'")
 
 bill = prices[size]['pizza']
 
-while True:
-    add_pepperoni = input("Do you want pepperoni? (yes or no)\n").lower()
-    if add_pepperoni in ['yes', 'no']:
-        break
-    print("invalid input, please enter 'yes' or 'no'")
+#* Ask for pepperoni
+add_pepperoni = get_yes_or_no("Do you want pepperoni? (yes or no)\n").lower()
+bill += prices[size]['pepperoni'] if add_pepperoni == 'yes' else 0
 
 
-if add_pepperoni == 'yes':
-    bill += prices[size]['pepperoni']
+#* Ask for extra cheese
+add_extra_cheese = get_yes_or_no("Do you want extra cheese? (yes or no)\n").lower()
+bill += prices[size]['extra_cheese'] if add_extra_cheese == 'yes' else 0
 
-while True:
-    add_extra_cheese = input("Do you want extra cheese? (yes or no)\n").lower()
-    if add_extra_cheese in ['yes', 'no']:
-        break
-    print("invalid input, please enter 'yes' or 'no'")
-
-if add_extra_cheese == 'yes':
-    bill += prices[size]['extra_cheese']
-
+#* prints the total price
 print(f"Your total bill is ${bill}")
