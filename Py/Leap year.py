dafd41c1bdcd3c577a58aae8a75052ba10db29d1
Kal-sh <kalecherinet@gmail.com>
2025-10-24 16:13:03 +0300

    Refactor leap year calculator and addition script for improved readability

--- Py/Leap year.py
+++ Py/Leap year.py
@@ -1,14 +1,17 @@
 
 #! leap year calculator
 
-yr = int(input('input year: '))
-if yr % 4 == 0:
-    if yr % 100 == 0:
-        if yr % 400 == 0:
-            print(f'the {yr} year is leap year')
-        else:
-            print('not leap year')
-    else:
-        print(f'{yr} is a leap year')
-else:
-    print('not a leap year')
+year = int(input('input year: '))
+# if yr % 4 == 0:
+#     if yr % 100 == 0:
+#         if yr % 400 == 0:
+#             print(f'the {yr} year is leap year')
+#         else:
+#             print('not leap year')
+#     else:
+#         print(f'{yr} is a leap year')
+# else:
+#     print('not a leap year')
+
+
+print(f'{year} is a leap year' if year % 4 == 0 and year % 100 != 0 else 'not a leap year')
